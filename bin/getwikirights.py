ff529875d7f0ca993af6705aa7eb3c9636009644
Antoine Musso <hashar@free.fr>
2019-02-22 09:20:15 +0100

    Add tox and flake8
    Fix reported issues:
    
    ./bin/getwikirights.py
    
    7:1: F401 'getpass' imported but unused
    11:1: E302 expected 2 blank lines, found 1
    23:16: E222 multiple spaces after operator
    25:5: F841 local variable 'wap' is assigned to but never used
    32:1: E305 expected 2 blank lines after class or function definition,
          found 1
    
    There is a very long string for an URL, ignores it with 'noqa'. The code
    should be refactored to pass a dict to requests.get.
    
    ./usertracker/UserTracker.py
    
    12:1: F401 'sys' imported but unused
    100:13: F841 local variable 'all_post_data' is assigned to but never used
    274:37: E117 over-indented
    
    Bug: T216801
    Change-Id: I9e5bdccdb9d5d2f36d671d06eade37384bcd8ba5

--- bin/getwikirights.py
+++ bin/getwikirights.py
@@ -4,14 +4,14 @@ try:
     import requests
 except ImportError:
     print('Install requests3 library')
-import getpass
 import sys
 import json
+
 
 def main():
     user = sys.argv[1]
 
-    req = 'https://meta.wikimedia.org/w/api.php?action=query&format=json&meta=globaluserinfo&guiuser={}&guiprop=groups%7Crights%7Cmerged'
+    req = 'https://meta.wikimedia.org/w/api.php?action=query&format=json&meta=globaluserinfo&guiuser={}&guiprop=groups%7Crights%7Cmerged'  # noqa
     req_user = req.format(user)
     r = requests.get(req_user)
     results = json.loads(r.content.decode('utf-8'))
@@ -20,14 +20,14 @@ def main():
     else:
         global_groups = ','.join(results['query']['globaluserinfo']['groups'])
         print('global: {}'.format(global_groups))
-        wikis =  results['query']['globaluserinfo']['merged']
+        wikis = results['query']['globaluserinfo']['merged']
 
-    wap = {}
     for w in wikis:
         if 'groups' in w:
             wiki = w['wiki']
             rights = ','.join(w['groups'])
             print("{}: {}".format(wiki, rights))
+
 
 if __name__ == '__main__':
     main()
